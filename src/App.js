f1feab5d50928371a339c68fcf501d851b1e45d9
Andressa Atelli <andressaatelli.programador@gmail.com>
2021-09-15 21:08:10 -0300

    adicionei botao

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Jumbotron, Container, Row, Col, Button, Form } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Jumbotron style={{
+      background: 'transparent !important',
+      backgroundColor: '#007bff',
+      padding: '5px',
+      margin: '5px',
+      width: '400px',
+     }}>
+       <Container>
+         <Row>
+           <Col xs='3'>
+             <Button variant='danger'>C</Button>
+           </Col>
+           <Col xs='9'>
+           </Col>
+         </Row>
+       </Container>
+    </Jumbotron>
   );
 }
 
